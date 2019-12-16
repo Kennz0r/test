477b761a1f8eb12026825cf2cf017df570df3e87
Kenneth Melhus <kenneth.melhus@gmail.com>
2019-12-16 21:36:42 +0100

    ny test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LARS SUGEeeee
+          LARS SUGEeeee balle
         </p>
         <p>
           D E SANT HEHE
