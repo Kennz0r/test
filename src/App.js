32f4c361ac029766b85a29f3492e6d8d517dfcfa
Kenneth Melhus <Kenneth.melhus@gmail.com>
2019-12-24 14:14:15 +0100

    l

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './meth.jpg';
+import logo from './yes.jpg';
 import './App.css';
 
 function App() {
@@ -7,12 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          FIX D JÆVLA NETTE DOKKA
-        </p>
-        <p>
-          SÅ HÅVARD KAN GANKA BOT
-        </p>
         <p>
           COPYRIGHT THUNDERCUNTS GANG 2019
         </p>
